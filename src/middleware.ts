2af89ca6194a172de12a6e4289203416085b6360
Muhammad <mir.abdulah@outlook.com>
2026-05-15 03:15:54 -0400

    Remove explicit runtime export from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export const runtime = 'edge'
+
 
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone()
